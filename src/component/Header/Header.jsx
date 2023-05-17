e6a8eec79b913133164dcb5a3c7ddf06995f3b52
Amin Ranjbar <amin.ranjbar.dev@gmail.com>
2023-05-17 22:28:44 +0200

    initial structure

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className='h-wrapper'>
+      <div className='h-container'>
+        <img src='./logo.png' alt='logo' width={100}/>
+
+        <div className='h-menu'>
+          <a href="">Residencies</a>
+          <a href="">Our value</a>
+          <a href="">About us</a>
+          <a href="">Get Started</a>
+          <button className = "button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default Header
