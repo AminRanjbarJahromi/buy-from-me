9a892040aad2535873464c7a5ad550e2011bac77
Amin Ranjbar <amin.ranjbar.dev@gmail.com>
2023-05-18 23:49:57 +0200

    work on css

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './Heads.css'
 const Header = () => {
   return (
     <section className='h-wrapper'>
-      <div className='h-container'>
+      <div className='flexCenter paddings innerWidth h-container'>
         <img src='./logo.png' alt='logo' width={100}/>
 
         <div className='h-menu'>
